ac4a39860d1128a0d7463a63d172a7584eebc924
Stephane Cottereau <stephane.cottereau@s4m.io>
2019-08-28 10:12:42 +0200

    WIP: APP SETUP: Basic arch, files, ready to implement vue appollo

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,26 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div
+    id="app"
+    class="app"
+  >
+    <div class="hdr-cmp">
+      <div class="hdr-cmp__logo" />
+      <h2 class="hdr-cmp__title">
+        Welcome to Apollo
+      </h2>
     </div>
-    <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<script>
+import logo from './assets/logo.svg';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      logo,
+    };
+  },
+};
+</script>
